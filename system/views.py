ea5401f6e98c09ea4fa9199d207338740756adb2
Gabriel Luiz <gabriells760@gmail.com>
2020-08-04 15:53:18 -0300

    Cadastro
    Commit Cadastro do usuário

--- system/views.py
+++ system/views.py
@@ -12,13 +12,43 @@ class Index(View):
 
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            nome = request.user.first_name.strip().split(' ')[0]
+        else:
+            nome = None
         self.contexto = {
             'users': request.user.is_authenticated,
-            'nome': request.user.first_name,
+            'nome': nome,
         }
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.contexto)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            nome = request.user.first_name.strip().split(' ')[0]
+        else:
+            nome = None
+
+        self.contexto = {
+            'users': request.user.is_authenticated,
+            'nome': nome,
+        }
+
+        if request.POST.get('email_cadastro'):
+            nome = request.POST.get('name')
+            email = request.POST.get('email_cadastro')
+            senha = request.POST.get('pwd')
+            senha2 = request.POST.get('pwd2')
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email já existe')
+            user = User.objects.create_user(username=email, email=email, password=senha, first_name=nome)
+            try:
+                user.save()
+            except:
+                messages.error(request, 'Falha ao se cadastrar! Contacte o administrador do site!')
+                return render(request, self.template_name, self.contexto)
 
 
 def login(request):
